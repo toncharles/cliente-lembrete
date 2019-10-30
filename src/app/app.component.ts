5681c18d47b6c8615471dd7f2870362c730f311e
Washington Charles de O Moraes <toncharles@live.com>
2019-10-30 21:43:23 -0200

    Adicionado Bootstrap ao projeto / correcao de importacao do service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cliente-lembrete';
+  titulo = 'CRUD de lembretes com o Angular';
 }
--- src/app/services/lembrete.service.ts
+++ src/app/services/lembrete.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Lembrete } from '../interfaces/lembrete';
 
